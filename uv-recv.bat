944f68a9209ca45657ef5351d1414d742ab372fe
Daniel Krikun <krikun.daniel@gmail.com>
2016-01-23 09:29:24 +0200

    Add UltraGrid examples

---
+++ uv-recv.bat
@@ -0,0 +1,8 @@
+
+@echo off
+
+set PATH=c:\proj\UltraGrid;%PATH%
+
+uv -P 10000 -d sdl 127.0.0.1
+pause
+
---
+++ uv-send.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set PATH=c:\proj\UltraGrid;%PATH%
+
+uv -P 10000 -t dshow:1 127.0.0.1
+pause
+
